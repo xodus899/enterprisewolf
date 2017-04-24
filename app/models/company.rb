3323aca525ec1983458441b0bc23f8e94c1b7d19
Christopher Justice <justcd@mail.broward.edu>
2017-04-24 18:01:05 -0400

    csv download and button

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,13 @@ class Company < ApplicationRecord
 			Company.create! row.to_hash
 		end
 	end
+
+	def self.to_csv
+		CSV.generate do |csv|
+			csv << column_names
+			all.each do |company|
+				csv << company.attributes.values_at(*column_names)
+			end
+		end
+	end
 end
