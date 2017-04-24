9f214b3e1f4aebfe90cea379372bb8a10fd56003
Christopher Justice <justcd@mail.broward.edu>
2017-04-24 12:08:20 -0400

    Add csv

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
  ===============
 
 
- Powerful software
+ Powerful  Invoice software
 
  -----------------
 
  ### Welcome to the repository
+
+ -----------------------------
+
+ ## With this software you will be able to manage your company invoices
 
 
  Feel free to access [my portfolio](http://Xodus899.github.io)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424143410) do
+ActiveRecord::Schema.define(version: 20170424152925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.string   "manager"
+    t.string   "status"
+    t.integer  "terms"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "invoices", force: :cascade do |t|
     t.datetime "date"
