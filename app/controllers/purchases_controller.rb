956c14e24655759ffc16ffc499391ae065a513d0
Christopher Justice <justcd@mail.broward.edu>
2017-04-24 23:56:37 -0400

    delete items function

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -58,10 +58,16 @@ class Invoices::PurchasesController < ApplicationController
   # DELETE /purchases/1
   # DELETE /purchases/1.json
   def destroy
-    @purchase.destroy
-    respond_to do |format|
-      format.html { redirect_to purchases_url, notice: 'Purchase was successfully destroyed.' }
-      format.json { head :no_content }
+    @invoice = Invoice.find(params[:invoice_id])
+    @purchase = Purchase.find(params[:id])
+    title = @purchase.name
+
+    if @purchase.destroy 
+      flash[:notice] = "\"#{title}\" was deleted successfully."
+    redirect_to @invoice
+    else
+      flash[:error] = "There was an issue removing your item"
+      render :show
     end
   end
 
